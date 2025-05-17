96e2de3bf73e4aef3186bf193c64e506d9338108
Daria Kudrina <fornicemice@gmail.com>
2025-05-17 20:57:58 +0300

    Минимальный рабочий проект готов

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import  Column, Integer, String
 from sqlalchemy.orm import Session
@@ -47,3 +48,24 @@ async def read_root(request: Request):
     return templates.TemplateResponse(
         "index1.html", {"request": request, "title": "Главная страница", "emails": all_emails}
     )
+
+@app.get("/email/{email_id}")
+async def read_email(request: Request, email_id: int):
+    # Создаем сессию
+    session = Session(bind=engine)
+
+    try:
+        # Получаем письмо по ID
+        email = session.query(Email).filter(Email.id == email_id).first()
+
+        if not email:
+            # Если письмо не найдено, перенаправляем на главную страницу
+            return RedirectResponse(url="/")
+
+        # Отображаем страницу с деталями письма
+        return templates.TemplateResponse(
+            "detail.html", {"request": request, "email": email}
+        )
+    finally:
+        # Закрываем сессию
+        session.close()
